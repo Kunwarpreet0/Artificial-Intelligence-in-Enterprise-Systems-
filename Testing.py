9ef88352cd3196f2d8f6393f1e4af950b1bb0368
Kunwarpreet0 <124310839+Kunwarpreet0@users.noreply.github.com>
2023-05-28 14:32:43 -0400

    First Commit

---
+++ Testing.py
@@ -0,0 +1,18 @@
+# getting my name
+name = input("Enter your name: ")
+
+print("Hello, " + name + "!")
+
+num1 = 10
+num2 = 5
+sum = num1 + num2
+
+# Printing the Sum
+print("The sum of", num1, "and", num2, "is:", sum)
+
+# If else statement
+if sum > 15:
+    print("The sum is greater than 15!")
+else:
+    print("The sum is not greater than 15.")
+
